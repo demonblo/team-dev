d9b4c528f321e16ed76d269b841b2d587e844ffa
Dmitry Blokhin <demonblo007@gmail.com>
2023-04-29 22:34:35 +0300

    add Item repository

---
+++ backend/warehouse/src/main/java/com/example/werehouse/repository/ItemRepository.java
@@ -0,0 +1,14 @@
+package com.example.werehouse.repository;
+
+import com.example.werehouse.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findAllByUserId(Long userId);
+    List<Item> findAllByNameContaining(String name);
+    List<Item> findAllByVendorCodeContaining(String name);
+}
