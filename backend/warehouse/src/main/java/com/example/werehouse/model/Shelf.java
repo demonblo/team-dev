4832df7408b3e08ccd474ed35091ff544df5b662
Dmitry Blokhin <demonblo007@gmail.com>
2023-04-29 22:18:34 +0300

    add Shelf model

---
+++ backend/warehouse/src/main/java/com/example/werehouse/model/Shelf.java
@@ -0,0 +1,24 @@
+package com.example.werehouse.model;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "shelf")
+public class Shelf {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "shelf_id_gen")
+    @SequenceGenerator(name="shelf_id_gen", sequenceName = "shelf_id_seq", allocationSize = 1)
+    private Long id;
+    private String number;
+    private Double maxWeight;
+    private Double freeWeight;
+}
